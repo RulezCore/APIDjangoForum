691c3ab0051f2817cfb52cf847c3d9000d3d7feb
Daniel Casado <ixrulez69@gmail.com>
2020-04-04 02:22:40 +0200

    User Profile implemented

--- foro/admin.py
+++ foro/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Category, Topic
+from .models import Category, Topic, Profile
 
 # Register your models here.
 admin.site.register(Category)
 admin.site.register(Topic)
+admin.site.register(Profile)
--- foro/models.py
+++ foro/models.py
@@ -1,7 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+    profilePic = models.ImageField(upload_to='profile_pics', blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+    
+
 
 class Category(models.Model):
     title = models.CharField(max_length=150, verbose_name="Titulo")
@@ -59,3 +72,14 @@ class ResponseToResponse(models.Model):
     textResponse = models.TextField()
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     created = models.DateField(auto_now_add=True)
+
+
+# Signals
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
